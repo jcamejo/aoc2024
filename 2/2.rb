ab9b5244525cc79037b34daa6728c48318a2581d
Juan Carlos Camejo <juan.camejo@carwow.es>
2024-12-02 20:00:36 +0100

    rework task 1

--- 2/2.rb
+++ 2/2.rb
@@ -8,40 +8,42 @@ def get_order(a,b)
   end
 end
 
-def safe_report?(report)
-  previous_level = nil
+def identify_bad_levels(report)
   main_order = nil
+  report.each_with_index do |level, i|
+    next_level = report[i+1]
+    next if next_level.nil?
 
-  outcome = report.all? do |level|
-    if previous_level.nil?
-      previous_level = level
-      next true
-    elsif previous_level == level
-      next false
-    elsif main_order.nil?
-      main_order = get_order(previous_level, level)
+    difference = level - next_level
+    if difference == 0
+      report[i] = nil 
+      next
     end
 
-    current_order = get_order(previous_level, level)
-    difference = previous_level - level
+    main_order = get_order(level, next_level) if main_order.nil?
+    current_order = get_order(level, next_level)
 
-    puts "difference #{previous_level} - #{level}", difference
+    if main_order != current_order
+      report[i] = nil 
+      next
+    end
 
-    if current_order == main_order && difference.between?(-3,3)
-      previous_level = level
-      next true
-    else
-      next false
+    if difference < -3 || difference > 3
+      report[i] = nil 
+      next
     end
   end
 
-  outcome
+  p "report", report
+  report
 end
 
 File.readlines("input", chomp: true)
   .map { |report| report.split(" ").map(&:to_i) }
   .then do |reports|
-    safe_reports = reports.select { |report| safe_report?(report) }
+    reports.map! { |report| identify_bad_levels(report) }
+
+    safe_reports = reports.select { |report| !report.include?(nil) }
 
     p "safe reports", safe_reports.length
   end
