ca37fd4bfb1ddf0b420c8a30f4edb075eb1aa364
Juan Carlos Camejo <juan.camejo@carwow.es>
2024-12-03 15:31:18 +0100

    task 2 part 2

--- 2/2.rb
+++ 2/2.rb
@@ -1,49 +1,67 @@
-def get_order(a,b)
-  if a < b
-    :asc
-  elsif a > b
-    :desc
-  else
-    nil
-  end
+def get_order(report)
+  report[0] > report[1] ? :desc : :asc
 end
 
-def identify_bad_levels(report)
-  main_order = nil
-  report.each_with_index do |level, i|
-    next_level = report[i+1]
-    next if next_level.nil?
+def valid_level?(report, current_level, next_level)
+  return true if next_level.nil?
 
-    difference = level - next_level
-    if difference == 0
-      report[i] = nil 
-      next
-    end
+  difference = current_level - next_level
 
-    main_order = get_order(level, next_level) if main_order.nil?
-    current_order = get_order(level, next_level)
+  return false if difference == 0
 
-    if main_order != current_order
-      report[i] = nil 
-      next
-    end
+  main_order = get_order(report)
 
-    if difference < -3 || difference > 3
-      report[i] = nil 
-      next
-    end
+  return true if main_order == :asc && difference.between?(-3, -1)
+  return true if main_order == :desc && difference.between?(1, 3)
+
+  false
+end
+
+def all_valid?(report)
+  match_valid(report).all?
+end
+
+def match_valid(report)
+  new_report = []
+
+  report.each_with_index do |level, i| 
+    new_report << valid_level?(report, level, report[i+1])
   end
 
-  p "report", report
-  report
+  new_report
+end
+
+def check_report(report, stack: 0, max_level: 2)
+  return false if stack == max_level
+
+  return true if all_valid?(report)
+
+  return false if stack+1 == max_level
+
+  report.each_with_index do |level, i|
+    new_report = report.dup.tap{|r| r.delete_at(i) }
+
+    return true if check_report(new_report, stack: stack+1)
+  end
+
+  false
 end
 
 File.readlines("input", chomp: true)
   .map { |report| report.split(" ").map(&:to_i) }
   .then do |reports|
-    reports.map! { |report| identify_bad_levels(report) }
+    safe_reports = reports.select do |report| 
+      check_report(report, max_level: 1)
+    end
 
-    safe_reports = reports.select { |report| !report.include?(nil) }
+    safe_reports_with_damper = reports.select do |report| 
+      check_report(report)
+    end
 
-    p "safe reports", safe_reports.length
+
+    # Task 1
+    pp safe_reports.length
+
+    # Task 2
+    pp safe_reports_with_damper.length
   end
